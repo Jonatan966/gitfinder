569867a793095a14e7b23a416bedc95270824fa1
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-02-13 16:56:34 -0300

    feat: create repository card component

---
+++ src/components/repository-card.tsx
@@ -0,0 +1,67 @@
+import { InfoIcon, StarIcon, ViewIcon } from '@chakra-ui/icons'
+import {
+  Avatar,
+  Box,
+  HStack,
+  Link,
+  Text,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react'
+
+export interface RepositoryCardProps {
+  name: string
+  ownerAvatarUrl: string
+  description?: string
+  stars: number
+  issues: number
+  mainLanguage: string
+  repoUrl: string
+}
+
+export function RepositoryCard({
+  name,
+  ownerAvatarUrl,
+  description,
+  stars,
+  issues,
+  mainLanguage,
+  repoUrl,
+}: RepositoryCardProps) {
+  return (
+    <Link href={repoUrl} target="_blank">
+      <Box
+        border="2px"
+        borderColor="gray.200"
+        p="2"
+        d="inline-block"
+        w="100%"
+        mb="2"
+      >
+        <HStack>
+          <Avatar size="sm" src={ownerAvatarUrl} name={name} />
+          <Text fontWeight="bold">{name}</Text>
+        </HStack>
+
+        <Wrap spacing="2" my="1">
+          <WrapItem alignItems="center" gap="1">
+            <StarIcon color="gray.600" />
+            <Text fontWeight="medium">{stars}</Text>
+          </WrapItem>
+
+          <WrapItem alignItems="center" gap="1">
+            <InfoIcon color="gray.600" />
+            <Text fontWeight="medium">{issues}</Text>
+          </WrapItem>
+
+          <WrapItem alignItems="center" gap="1">
+            <ViewIcon color="gray.600" />
+            <Text fontWeight="medium">{mainLanguage}</Text>
+          </WrapItem>
+        </Wrap>
+
+        <Text>{description}</Text>
+      </Box>
+    </Link>
+  )
+}
