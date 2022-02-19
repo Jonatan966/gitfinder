6f5c0fcf61f357b965d7e6804e33eb90305a1ee6
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-02-19 11:55:40 -0300

    feat: create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
+import { Flex } from '@chakra-ui/react'
+import { Header } from './components/header'
+import { RepositoryList } from './components/repository-list'
+import { SearchMessage } from './components/messages/search-message'
+import { useRepoSearch } from './contexts/repo-search'
+import { NotFoundMessage } from './components/messages/not-found-message'
+
 function App() {
+  const { isInSearchMode, isSearching, searchResult } = useRepoSearch()
+
+  function renderContent() {
+    if (!isInSearchMode) {
+      return <SearchMessage />
+    }
+
+    if (isSearching || searchResult.totalRepos > 0) {
+      return <RepositoryList />
+    }
+
+    return <NotFoundMessage />
+  }
+
   return (
-    <div className="App">
-      <h1>Bom dia</h1>
-    </div>
+    <Flex
+      direction="column"
+      maxW={1024}
+      mx="auto"
+      px="4"
+      w="100%"
+      minH="100vh"
+      h="100%"
+    >
+      <Header />
+
+      {renderContent()}
+    </Flex>
   )
 }
 
