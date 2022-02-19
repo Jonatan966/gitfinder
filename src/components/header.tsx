872c9983a2111d075497a85b6007dfa5c5e8c9dd
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-02-19 11:53:40 -0300

    feat: create repo search mechanism

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,8 +7,24 @@ import {
   Button,
   Text,
 } from '@chakra-ui/react'
+import { useRef } from 'react'
+import { useRepoSearch } from '../contexts/repo-search'
 
 export function Header() {
+  const searchInputRef = useRef<HTMLInputElement>(
+    document.createElement('input')
+  )
+  const { search, isInSearchMode, clear, isSearching } = useRepoSearch()
+
+  function handleSearch() {
+    search(searchInputRef.current.value)
+  }
+
+  function handleClear() {
+    searchInputRef.current.value = ''
+    clear()
+  }
+
   return (
     <HStack
       as="header"
@@ -30,11 +46,35 @@ export function Header() {
           w="100%"
           borderRadius="0"
           placeholder="Pesquise por um nome de um repositório"
+          ref={searchInputRef}
+          disabled={isInSearchMode}
+          onKeyPress={(event) => {
+            if (event.key === 'Enter') {
+              handleSearch()
+            }
+          }}
         />
       </InputGroup>
-      <Button w="36" colorScheme="yellow" borderRadius="0">
-        Buscar
-      </Button>
+      {isInSearchMode && !isSearching ? (
+        <Button
+          w="36"
+          colorScheme="red"
+          borderRadius="0"
+          onClick={() => handleClear()}
+        >
+          Limpar
+        </Button>
+      ) : (
+        <Button
+          w="36"
+          colorScheme="yellow"
+          borderRadius="0"
+          onClick={() => handleSearch()}
+          isLoading={isSearching}
+        >
+          Buscar
+        </Button>
+      )}
     </HStack>
   )
 }
