735fcc972f8e58e52fbe599f0ef1cc98de01d9f9
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-02-19 11:16:29 -0300

    feat: add axios, react query and list manager

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClientProvider, QueryClient } from 'react-query'
 import App from './App'
+import { RepoSearchProvider } from './contexts/repo-search'
+
+export const queryClient = new QueryClient()
 
 ReactDOM.render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <RepoSearchProvider>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </RepoSearchProvider>
+    </QueryClientProvider>
   </StrictMode>,
   document.getElementById('root')
 )
